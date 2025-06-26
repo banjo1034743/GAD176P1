76877490abaf35f6029f594be22a414f86690e87
Banjo - Home Laptop <1034743@student.sae.edu.au>
2025-06-26 19:06:18 +1000

    Last changes for prior commit
    Unsaved changes for prior commit

--- Assets/Scripts/EnemyAI/EnemyAI.cs
+++ Assets/Scripts/EnemyAI/EnemyAI.cs
@@ -6,10 +6,14 @@ namespace SAE.GAD176.P1.EnemyAI
 {
     public abstract class EnemyAI : MonoBehaviour, IIdleFunctionality, IAttackFunctionality, IFleeFunctionality, IHealthFunctionality, IKillableFunctionality, IPlayerCheckerFunctionality
     {
+        [Header("Scripts")]
+
         [SerializeField] protected IdleStateManager idleState;
         [SerializeField] protected PlayerSightedChecker playerSightedChecker;
         [SerializeField] protected FleeStateManager fleeState;
         [SerializeField] protected HealthManager healthManager;
+
+        [Header("Data")]
 
         protected bool isIdleStateEnabled = false;
         
@@ -23,6 +27,7 @@ namespace SAE.GAD176.P1.EnemyAI
 
         public bool PlayerInSightCheck()
         {
+
             return isIdleStateEnabled;
         }
 
